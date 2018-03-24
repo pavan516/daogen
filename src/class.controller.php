06248ed88c7b099b0ca17476d755004cad14fb41
Kim Sandell <kimsandell73@gmail.com>
2018-03-24 13:47:41 +0100

    Controller generation: POST, PUT fixed to be smarter/better

--- src/class.controller.php
+++ src/class.controller.php
@@ -103,19 +103,21 @@ class Controller
     $s .= "class ".$this->table->getClassName()."Controller extends ".( !empty($this->extends) ? $this->extends : 'Controller').PHP_EOL;
     $s .= "{".PHP_EOL;
 
-    # Initialize
-    $s .= ' /**'.PHP_EOL;
-    $s .= '   * Initialize Controller'.PHP_EOL;
-    $s .= '   *'.PHP_EOL;
-    $s .= '   * @param      array   $args   Path variables as key=value array'.PHP_EOL;
-    $s .= '   */'.PHP_EOL;
-    $s .= '  public function initialize(array $args)'.PHP_EOL;
-    $s .= '  {'.PHP_EOL;
-    $s .= '    parent::initialize($args);'.PHP_EOL;
-    $s .= '  }'.PHP_EOL;
-    $s .= PHP_EOL;
+    // # Initialize
+    // $s .= ' /**'.PHP_EOL;
+    // $s .= '   * Initialize Controller'.PHP_EOL;
+    // $s .= '   *'.PHP_EOL;
+    // $s .= '   * @param      array   $args   Path variables as key=value array'.PHP_EOL;
+    // $s .= '   */'.PHP_EOL;
+    // $s .= '  public function initialize(array $args)'.PHP_EOL;
+    // $s .= '  {'.PHP_EOL;
+    // $s .= '    parent::initialize($args);'.PHP_EOL;
+    // $s .= '  }'.PHP_EOL;
+    // $s .= PHP_EOL;
 
+    #
     # GET
+    #
     $s .= '  /**'.PHP_EOL;
     $s .= '   * Handle GET requests'.PHP_EOL;
     $s .= '   *'.PHP_EOL;
@@ -127,20 +129,20 @@ class Controller
     $s .= '  {'.PHP_EOL;
     if ($this->table->hasField('uuid')) {
       $s .= '    $items = null;'.PHP_EOL;
-      $s .= '    $parUuid = $args[\'uuid\'] ?? null;'.PHP_EOL;
+      $s .= '    $par_uuid = $args[\'uuid\'] ?? null;'.PHP_EOL;
       $s .= PHP_EOL;
-      $s .= '    if (!empty($parUuid)) {'.PHP_EOL;
-      $s .= '      $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'uuid\',$parUuid);'.PHP_EOL;
+      $s .= '    if (!empty($par_uuid)) {'.PHP_EOL;
+      $s .= '      $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'uuid\',$par_uuid);'.PHP_EOL;
       $s .= '      if ($item) $items[] = $item;'.PHP_EOL;
       $s .= '    } else {'.PHP_EOL;
       $s .= '      $items = (new '.$this->table->getClassName().'Dao())->fetchAll();'.PHP_EOL;
       $s .= '    }'.PHP_EOL;
     } else
     if ($this->table->hasField('code')) {
-      $s .= '    $parCode = $args[\'code\'] ?? null;'.PHP_EOL;
+      $s .= '    $par_code = $args[\'code\'] ?? null;'.PHP_EOL;
       $s .= PHP_EOL;
-      $s .= '    if (!empty($parCode)) {'.PHP_EOL;
-      $s .= '      $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'code\',$parCode);'.PHP_EOL;
+      $s .= '    if (!empty($par_code)) {'.PHP_EOL;
+      $s .= '      $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'code\',$par_code);'.PHP_EOL;
       $s .= '      if ($item) $items[] = $item;'.PHP_EOL;
       $s .= '    } else {'.PHP_EOL;
       $s .= '      $items = (new '.$this->table->getClassName().'Dao())->fetchAll();'.PHP_EOL;
@@ -161,7 +163,9 @@ class Controller
     $s .= '  }'.PHP_EOL;
     $s .= PHP_EOL;
 
+    #
     # POST
+    #
     $s .= '  /**'.PHP_EOL;
     $s .= '   * Handle POST requests'.PHP_EOL;
     $s .= '   *'.PHP_EOL;
@@ -182,30 +186,63 @@ class Controller
     $s .= '      return responseJsonError(\'Bad request\',\'Invalid post body\',400);'.PHP_EOL;
     $s .= '    }'.PHP_EOL;
     $s .= PHP_EOL;
-    $s .= '    // Should check for existance of $item in DB'.PHP_EOL;
-    $s .= '    // Ex. using: '.PHP_EOL;
-    $s .= '    //   $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'id\',$id);'.PHP_EOL;
+
+    $s .= '    # Extract & Verify params'.PHP_EOL;
+    if ($this->table->hasField('uuid')) {
+      $s .= '    $par_uuid = $body[\'uuid\'] ?? null;'.PHP_EOL;
+      $s .= '    if (empty($par_uuid)) {'.PHP_EOL;
+      $s .= '      return responseJsonError(\'Bad request\',\'{uuid} must be specified\',400);'.PHP_EOL;
+      $s .= '    }'.PHP_EOL;
+    }
+    if ($this->table->hasField('code')) {
+      $s .= '    $par_code = $body[\'code\'] ?? null;'.PHP_EOL;
+      $s .= '    if (empty($par_code)) {'.PHP_EOL;
+      $s .= '      return responseJsonError(\'Bad request\',\'{code} must be specified\',400);'.PHP_EOL;
+      $s .= '    }'.PHP_EOL;
+    }
+    if ($this->table->hasField('name')) {
+      $s .= '    $par_name = $body[\'name\'] ?? null;'.PHP_EOL;
+      $s .= '    if (empty($par_name)) {'.PHP_EOL;
+      $s .= '      return responseJsonError(\'Bad request\',\'{name} must be specified\',400);'.PHP_EOL;
+      $s .= '    }'.PHP_EOL;
+    }
+    $s .= '    // .. there are more fields that need verification?'.PHP_EOL;
     $s .= PHP_EOL;
 
-    $s .= '    # Create new Item, set properties'.PHP_EOL;
+    $s .= '    # Check if the item exists'.PHP_EOL;
+    if ($this->table->hasField('uuid')) {
+      $s .= '    $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'uuid\',$par_uuid);'.PHP_EOL;
+      $s .= '    if ( isset($item) ) {'.PHP_EOL;
+      $s .= '      return responseJsonError(\'Bad request\',\'An item with {uuid} already exists\',400);'.PHP_EOL;
+      $s .= '    }'.PHP_EOL;
+    } else
+    if ($this->table->hasField('code')) {
+      $s .= '    $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'code\',$par_code);'.PHP_EOL;
+      $s .= '    if ( isset($item) ) {'.PHP_EOL;
+      $s .= '      return responseJsonError(\'Bad request\',\'An item with {code} already exists\',400);'.PHP_EOL;
+      $s .= '    }'.PHP_EOL;
+    }
+    $s .= '    // .. there might be other fields that need existance check?'.PHP_EOL;
+    $s .= PHP_EOL;
+
+    $s .= '    # Create new Item from body, Override some properties'.PHP_EOL;
     $s .= '    $item = new '.$this->table->getClassName().'Entity($body);'.PHP_EOL;
     foreach ($this->table->getFields() as $field) {
       if ( strcasecmp($field->getName(),'uuid')==0) {
-        $s .= '    // $item->set'.$field->getUcwName().'( UUID::generate() ); // Generate a UUID v4'.PHP_EOL;
+        $s .= '    $item->set'.$field->getUcwName().'( UUID::generate() ); // Generate a UUID v4'.PHP_EOL;
       } elseif ($field->isDateTime()) {
-        $s .= '    // $item->set'.$field->getUcwName().'( (new \DateTime(\'now\',new \DateTimeZone(\'UTC\')))->format(\'Y-m-d\TH:i:s\Z\') ); // UTC Date Time'.PHP_EOL;
+        $s .= '    $item->set'.$field->getUcwName().'( (new \DateTime(\'now\',new \DateTimeZone(\'UTC\')))->format(\'Y-m-d\TH:i:s\Z\') ); // UTC Date Time'.PHP_EOL;
       } else {
         $s .= '    // $item->set'.$field->getUcwName().'('.$field->getDefault('php').');'.PHP_EOL;
       }
     }
     $s .= PHP_EOL;
+    $s .= '    # Save/Insert'.PHP_EOL;
     $s .= '    $ok = (new '.$this->table->getClassName().'Dao())->insert($item);'.PHP_EOL;
     $s .= PHP_EOL;
     $s .= '    if (!$ok) {'.PHP_EOL;
     $s .= PHP_EOL;
-    $s .= '      logger()->error(\'Failed to insert into database\','.PHP_EOL;
-    $s .= '        [\'rid\'=>container(\'requestId\'), \'item\'=>$item->asArray()]'.PHP_EOL;
-    $s .= '      );'.PHP_EOL;
+    $s .= '      logger()->error(\'Failed to insert into database\',[\'rid\'=>container(\'requestId\'), \'item\'=>$item->asArray()]);'.PHP_EOL;
     $s .= PHP_EOL;
     $s .= '      return responseJsonError(\'Failed to insert item\',\'\',500);'.PHP_EOL;
     $s .= '    }'.PHP_EOL;
@@ -214,7 +251,9 @@ class Controller
     $s .= '  }'.PHP_EOL;
     $s .= PHP_EOL;
 
+    #
     # PUT
+    #
     $s .= '  /**'.PHP_EOL;
     $s .= '   * Handle PUT requests'.PHP_EOL;
     $s .= '   *'.PHP_EOL;
@@ -225,13 +264,33 @@ class Controller
     $s .= '  public function handlePUT(array $args)'.PHP_EOL;
     $s .= '  {'.PHP_EOL;
     if ($this->table->hasField('uuid')) {
-      $s .= '    $parUuid = $args[\'uuid\'] ?? null;'.PHP_EOL;
+      $s .= '      # The uuid we want to update'.PHP_EOL;
+      $s .= '      $par_old_uuid = $args[\'uuid\'] ?? null;'.PHP_EOL;
       $s .= PHP_EOL;
+      $s .= '      if (empty($par_old_uuid) ) {'.PHP_EOL;
+      $s .= '        return responseJsonError(\'Bad request\',\'Query parameter {uuid} must be specified\',400);'.PHP_EOL;
+      $s .= '      }'.PHP_EOL;
+      $s .= PHP_EOL;
+      $s .= '      $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'uuid\',$par_old_uuid);'.PHP_EOL;
+      $s .= '      if (!$item) {'.PHP_EOL;
+      $s .= '        return responseJsonError(\'Not found\',\'Item with {uuid} not found\',404);'.PHP_EOL;
+      $s .= '      }'.PHP_EOL;
     } else
     if ($this->table->hasField('code')) {
-      $s .= '    $parCode = $args[\'code\'] ?? null;'.PHP_EOL;
+      $s .= '      # The code we want to update'.PHP_EOL;
+      $s .= '      $par_old_code = $args[\'code\'] ?? null;'.PHP_EOL;
       $s .= PHP_EOL;
+      $s .= '      if (empty($par_old_code) ) {'.PHP_EOL;
+      $s .= '        return responseJsonError(\'Bad request\',\'Query parameter {code} must be specified\',400);'.PHP_EOL;
+      $s .= '      }'.PHP_EOL;
+      $s .= PHP_EOL;
+      $s .= '      $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'code\',$par_old_code);'.PHP_EOL;
+      $s .= '      if (!$item) {'.PHP_EOL;
+      $s .= '        return responseJsonError(\'Not found\',\'Item with {code} not found\',404);'.PHP_EOL;
+      $s .= '      }'.PHP_EOL;
     }
+    $s .= PHP_EOL;
+
     $s .= '    # Validate HTTP Request "Content-type"'.PHP_EOL;
     $s .= '    if (!preg_match(\'/application\/json/i\',(getRequest()->getHeader(\'Content-Type\')[0] ?? \'\'))) {'.PHP_EOL;
     $s .= '      return responseJsonError(\'Bad request\',\'Content-Type must be "application-json"\',400);'.PHP_EOL;
@@ -244,28 +303,73 @@ class Controller
     $s .= '    }'.PHP_EOL;
     $s .= PHP_EOL;
 
+    $s .= '    # Extract & Verify params'.PHP_EOL;
     if ($this->table->hasField('uuid')) {
-      $s .= '    $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'uuid\',$parUuid);'.PHP_EOL;
+      $s .= '    $par_uuid = $body[\'uuid\'] ?? null;'.PHP_EOL;
+      $s .= '    if (empty($par_uuid)) {'.PHP_EOL;
+      $s .= '      return responseJsonError(\'Bad request\',\'{uuid} must be specified\',400);'.PHP_EOL;
+      $s .= '    }'.PHP_EOL;
+    }
+    if ($this->table->hasField('code')) {
+      $s .= '    $par_code = $body[\'code\'] ?? null;'.PHP_EOL;
+      $s .= '    if (empty($par_code)) {'.PHP_EOL;
+      $s .= '      return responseJsonError(\'Bad request\',\'{code} must be specified\',400);'.PHP_EOL;
+      $s .= '    }'.PHP_EOL;
+    }
+    if ($this->table->hasField('name')) {
+      $s .= '    $par_name = $body[\'name\'] ?? null;'.PHP_EOL;
+      $s .= '    if (empty($par_name)) {'.PHP_EOL;
+      $s .= '      return responseJsonError(\'Bad request\',\'{name} must be specified\',400);'.PHP_EOL;
+      $s .= '    }'.PHP_EOL;
+    }
+    $s .= '    // .. there are more fields that need verification?'.PHP_EOL;
+    $s .= PHP_EOL;
+
+    if ($this->table->hasField('uuid')) {
+      // $s .= '    # Check if new uuid exists (only if different from old)'.PHP_EOL;
+      // $s .= '    if ( strcasecmp($par_uuid, $par_old_uuid) != 0) {'.PHP_EOL;
+      // $s .= '      $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'uuid\',$par_uuid);'.PHP_EOL;
+      // $s .= '      if ( isset($item) ) {'.PHP_EOL;
+      // $s .= '        return responseJsonError(\'Bad request\',\'An item with {post.uuid} already exists\',400);'.PHP_EOL;
+      // $s .= '      }'.PHP_EOL;
     } else
     if ($this->table->hasField('code')) {
-      $s .= '    $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'code\',$parCode);'.PHP_EOL;
-    } else {
-      $s .= '    $item = new '.$this->table->getClassName().'Entity($body);'.PHP_EOL;
+      $s .= '    # Check if new code exists (only if different from old)'.PHP_EOL;
+      $s .= '    if ( strcasecmp($par_code, $par_old_code) != 0) {'.PHP_EOL;
+      $s .= '      $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'code\',$par_code);'.PHP_EOL;
+      $s .= '      if ( isset($item) ) {'.PHP_EOL;
+      $s .= '        return responseJsonError(\'Bad request\',\'An item with {post.code} already exists\',400);'.PHP_EOL;
+      $s .= '      }'.PHP_EOL;
     }
     $s .= PHP_EOL;
 
-    $s .= '    # Set all fields from $body'.PHP_EOL;
+    $s .= '    # Load the item'.PHP_EOL;
+    if ($this->table->hasField('uuid')) {
+      $s .= '    $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'uuid\',$par_old_uuid);'.PHP_EOL;
+    } else
+    if ($this->table->hasField('code')) {
+      $s .= '    $item = (new '.$this->table->getClassName().'Dao())->fetchBy(\'code\',$par_old_code);'.PHP_EOL;
+    }
+    $s .= PHP_EOL;
+
+    $s .= '    # Set properties from $body'.PHP_EOL;
     foreach ($this->table->getFields() as $field) {
-      $s .= '    // $item->set'.$field->getUcwName().'($body[\''.$field->getName().'\']);'.PHP_EOL;
+      if ( strcasecmp($field->getName(),'uuid')==0) {
+        $s .= '    // $item->set'.$field->getUcwName().'($body[\''.$field->getName().'\']);'.PHP_EOL;
+      } else {
+      if ( strcasecmp($field->getName(),'id')==0) {
+        $s .= '    // $item->set'.$field->getUcwName().'($body[\''.$field->getName().'\']);'.PHP_EOL;
+      } else {
+        $s .= '    $item->set'.$field->getUcwName().'($body[\''.$field->getName().'\']);'.PHP_EOL;
+      }
     }
     $s .= PHP_EOL;
 
+    $s .= '    # Save/Update'.PHP_EOL;
     $s .= '    $ok = (new '.$this->table->getClassName().'Dao())->update($item);'.PHP_EOL;
     $s .= PHP_EOL;
     $s .= '    if (!$ok) {'.PHP_EOL;
-    $s .= '      logger()->error(\'Failed to update in database\','.PHP_EOL;
-    $s .= '        [\'rid\'=>app(\'requestId\'), \'item\'=>$item->asArray()]'.PHP_EOL;
-    $s .= '      );'.PHP_EOL;
+    $s .= '      logger()->error(\'Failed to update in database\',[\'rid\'=>app(\'requestId\'), \'item\'=>$item->asArray()]);'.PHP_EOL;
     $s .= PHP_EOL;
     $s .= '      return responseJsonError(\'Failed to update item\',\'\',500);'.PHP_EOL;
     $s .= '    }'.PHP_EOL;
@@ -274,7 +378,10 @@ class Controller
     $s .= '  }'.PHP_EOL;
     $s .= PHP_EOL;
 
+
+    #
     # DELETE
+    #
     $s .= '  /**'.PHP_EOL;
     $s .= '   * Handle DELETE requests'.PHP_EOL;
     $s .= '   *'.PHP_EOL;
@@ -293,7 +400,7 @@ class Controller
       $s .= '    }'.PHP_EOL;
     } else
     if ($this->table->hasField('code')) {
-      $s .= '    $parCode = $args[\'code\'] ?? null;'.PHP_EOL;
+      $s .= '    $par_code = $args[\'code\'] ?? null;'.PHP_EOL;
       $s .= PHP_EOL;
       $s .= '    # Sanity check'.PHP_EOL;
       $s .= '    if (empty($par_code) ) {'.PHP_EOL;
