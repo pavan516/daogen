9bbed57b998c5fa2b224dd1fcc5719fb98bcc6b5
Kim Sandell <kimsandell73@gmail.com>
2018-03-25 17:12:16 +0200

    Minor fix to handleDELETE()

--- src/class.controller.php
+++ src/class.controller.php
@@ -359,6 +359,11 @@ class Controller
     } else
     if ($this->table->hasField('code')) {
       $s .= '    $entity = (new '.$this->table->getClassName().'Dao())->fetchBy(\'code\',$args[\'code\']);'.PHP_EOL;
+    } else
+    if ($this->table->hasField('sessionid')) {
+      $s .= '    $entity = (new '.$this->table->getClassName().'Dao())->fetchBy(\'sessionid\',$args[\'sessionid\']);'.PHP_EOL;
+    } else {
+      $s .= '    $entity = (new '.$this->table->getClassName().'Dao())->fetchBy(\'id\',$args[\'id\']);'.PHP_EOL;
     }
     $s .= PHP_EOL;
 
@@ -445,20 +450,18 @@ class Controller
     $s .= '    if ($response) return $response;'.PHP_EOL;
     $s .= PHP_EOL;
 
+    $s .= '    # Load the Entity to delete'.PHP_EOL;
     if ($this->table->hasField('uuid')) {
       $s .= '    $entity = (new '.$this->table->getClassName().'Dao())->fetchBy(\'uuid\',$args[\'uuid\']);'.PHP_EOL;
-      $s .= PHP_EOL;
-      $s .= '    if (!$entity) {'.PHP_EOL;
-      $s .= '      return responseJsonError(\'Not found\',\'{uuid} not found\',404);'.PHP_EOL;
-      $s .= '    }'.PHP_EOL;
     } else
     if ($this->table->hasField('code')) {
       $s .= '    $entity = (new '.$this->table->getClassName().'Dao())->fetchBy(\'code\',$args[\'code\']);'.PHP_EOL;
-      $s .= PHP_EOL;
-      $s .= '    if (!$entity) {'.PHP_EOL;
-      $s .= '      return responseJsonError(\'Not found\',\'{code} not found\',404);'.PHP_EOL;
-      $s .= '    }'.PHP_EOL;
     }
+    $s .= PHP_EOL;
+    $s .= '    # Report error if no entity found'.PHP_EOL;
+    $s .= '    if (!$entity) {'.PHP_EOL;
+    $s .= '      return responseJsonError(\'Not found\',\'Not found\',404);'.PHP_EOL;
+    $s .= '    }'.PHP_EOL;
     $s .= PHP_EOL;
     $s .= '    # Delete'.PHP_EOL;
     $s .= '    $ok = (new '.$this->table->getClassName().'Dao())->delete($entity);'.PHP_EOL;
